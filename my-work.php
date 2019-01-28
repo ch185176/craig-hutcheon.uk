a4617031b8ff2971415e3a5e24b0c9a9b3a89a5e
captaincarrot3 <craighutcheon333@hotmail.com>
2019-01-28 23:11:50 +0000

    Basic work feed

--- my-work.php
+++ my-work.php
@@ -10,12 +10,38 @@
     <body>
         <?php require_once 'includes/nav.php' ?>
         <div class="container" style="margin-top:10px">
+            <div class="row">
+                <?php
+                    $MyWorkPage = $ContentRepo->retrieveMyWork();
+                ?>
+                <h1><?=$MyWorkPage->heading?></h1>               
+            </div>
+            <div class="row">
+                <h2><?=$MyWorkPage->sub_heading?></h2>
+            </div>
+            <div class="row">
+                <p><?=$markdown->defaultTransform($MyWorkPage->content);?></p>
+            </div>
             <?php
-                $MyWorkPage = $ContentRepo->retrieveMyWork();
+                $Articles = $ContentRepo->retrieveArticles();
+                
+                foreach($Articles as $Article)
+                {
+                    $thumbnail = $Article->thumbnail->path;
+                    $title = $Article->title;
+                    $description = $Article->description;
+                    echo('<div class="row">');
+                    echo('<div class="col-sm-4">');
+                    echo('<h3>'. $title .'</h3>');
+                    echo('</div>');
+                    echo('<div class="col-sm-4">');
+                    echo('<img src="https://craig-hutcheon.uk/'. $thumbnail .'" alt="'. $title .'" class="img-thumbnail">');
+                    echo('</div>');
+                    echo('<div class="col-sm-4">');
+                    echo('<p>'. $markdown->defaultTransform($description) .'</p>');
+                    echo('</div>');
+                }
             ?>
-            <h1><?=$MyWorkPage->heading?></h1>
-            <h2><?=$MyWorkPage->sub_heading?></h2>
-            <p><?=$markdown->defaultTransform($MyWorkPage->content);?></p>
         </div>
     </body>
     <footer>
